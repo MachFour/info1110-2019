9438c6dbb13b82b9961b0655b067138e0cf81de2
Max Fisher <machfour+git@protonmail.com>
2019-04-15 21:07:30 +1000

    update wk7 av_len

--- week7/max/av_len.py
+++ week7/max/av_len.py
@@ -1,24 +1,22 @@
 # separate function to find total length of words
 def total_length(words):
     sum_length = 0
-    index = 0
-    while index < len(words):
-        word = words[index]
+    i = 0
+    while i < len(words):
+        word = words[i]
         if not isinstance(word, str):
-            raise ValueError("Error: {} at index {} is not a string".format(word, index))
-        sum_length += len(words[index])
-        index += 1
+            raise ValueError("Error: {} at index {} is not a string".format(word, i))
+        sum_length += len(words[i])
+        i += 1
     return sum_length
 
 def av_len(words):
     if len(words) == 0:
         return 0
-    index = 0
     # we guaranteed that that len(words) != 0,
     # so we are definitely not dividing by 0 here.
     average_length = total_length(words)/len(words)
     return average_length
-
 
 def test1():
     import sys
@@ -32,8 +30,6 @@ def test2():
     words = ("these", "are", "some", 0, "words")
     avg = av_len(words)
     print("Average length:", avg)
-
-
 
 # use this to test various inputs
 if __name__ == "__main__":
