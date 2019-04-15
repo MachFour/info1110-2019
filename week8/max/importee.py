59e8bacaa555754c6c5f707c39ce87c6da283fbd
Max Fisher <machfour+git@protonmail.com>
2019-04-15 22:08:31 +1000

    tweaked import example

--- week8/max/importee.py
+++ week8/max/importee.py
@@ -10,21 +10,23 @@ def do_something():
 
     r = random.random() # random float between 0 and 1
     a, b = r.as_integer_ratio() # approximate as p/q, where p, q are integers
-    answer = a ^ b # bitwise xor
-    print("I did something and the answer was {:#04x}.".format(answer))
+    ans = a ^ b # bitwise xor
 
-def say_hi():
+    print("Importee: I did something and the answer was {:#04x}.".format(ans))
+
+def say_hi_main():
+    print("Importee: ooh, I am being run as a script!")
+    print('''Let's do something :D''') # triple quotes are useful!
+    print()
+
+def say_hi_import():
     print("Importee: I am being imported! Yay!")
     print('I won\'t do anything yet...') # notice the escaped quote
     print()
 
 if __name__ == "__main__":
-    print("Importee: ooh, I am being run as a script!")
-    print('''Let's do something :D''') # triple quotes are useful!
-    print()
-
+    say_hi_main()
     do_something()
-
 else:
-    say_hi()
+    say_hi_import()
 
--- week8/max/importer.py
+++ week8/max/importer.py
@@ -2,18 +2,28 @@
 message = "This is Importer. I'm going to import the Importee, " \
     "because that is my job."
 
+# prints a string, prefixed by "Importer: ", and an extra newline afterwards
+def importer_print(s):
+    print("Importer: ", end="")
+    print(s)
+    print()
+
+
 def main():
     # Here, 'message' is a global variable
     # we don't need the 'global' keyword because we aren't trying to modify it.
-    print(message)
-    print()
+    importer_print(message)
 
     import importee
     # After importing the Importee module, we gain access to its functions
     # and variables. Importing this way means that they are prefixed by the
     # module name (importee)
-    print("Importer: The importee has another message:")
+    importer_print("The importee has another message:")
     print("'{:s}'".format(importee.message)) # print out a string in quotes
+
+    print()
+    importer_print("I will now ask the importee to do something...")
+    importee.do_something()
 
 # This is a very common pattern/idiom in Python
 # it means that all 'actual' code is inside a function.
