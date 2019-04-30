200aacd3f2ddd3cc9f0e27d8fc16e01762b8a47b
Max Fisher <machfour+git@protonmail.com>
2019-04-30 15:30:10 +1000

    fix player code

--- week9/T14C/player.py
+++ week9/T14C/player.py
@@ -1,11 +1,14 @@
 class Player:
     def __init__(self, name, score):
-        self.name = name
-        self.score = score
+        self._name = name
+        self._score = score
 
     # this is a getter function
     def score(self):
-        return self.score
+        return self._score
+
+    def name(self):
+        return self._name
 
     def best_score(players):
         best = players[0]
@@ -13,6 +16,7 @@ class Player:
         while i < len(players):
             if players[i].score() > best.score():
                 best = players[i]
+            i += 1
         return best
 
 if __name__ == "__main__":
@@ -21,5 +25,5 @@ if __name__ == "__main__":
     p3 = Player('Phineas', 150)
     ls = [p1, p2, p3]
     best = Player.best_score(ls)
-    msg = "{} has the best score, with {} points!".format(best.name, best.score())
+    msg = "{} has the best score, with {} points!".format(best.name(), best.score())
     print(msg)
--- week9/T14C/players.py
+++ week9/T14C/players.py
@@ -2,7 +2,9 @@ import player
 p4 = player.Player('Brett', 500)
 p5 = player.Player('Nancy', 2050)
 p6 = player.Player('Phillip', 1150)
+# this is a private variable, shouldn't be changed
+#p4._score = 4 # bad!
 ls = [p4, p5, p6]
 best = player.Player.best_score(ls)
-msg = "{} has the best score, with {} points!".format(best.name, best.score())
+msg = "{} has the best score, with {} points!".format(best.name(), best.score())
 print(msg)
