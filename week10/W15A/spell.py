854d19ffa5d9bff5d37820b9dcc00f664829f680
bitnahian <nhas9102@uni.sydney.edu.au>
2019-05-08 16:34:17 +1000

    Implemented getter and setter functions

--- week10/W15A/spell.py
+++ week10/W15A/spell.py
@@ -26,10 +26,41 @@ class Spell:
         self.level = level
         self.cast_limit = cast_limit
         self.effect = effect
-        self.casts = cast_limit
+        self.__casts = cast_limit
 
     def __repr__(self):
         """
         Returns a string to be outputted to console
         """
-        return "Spell name: {}, school: {}, level: {}, cast_limit: {}, effect: {}, casts: {}".format(self.name, self.school, self.level, self.cast_limit, self.effect, self.casts)
+        return "Spell name: {}, school: {}, level: {}, cast_limit: {}, effect: {}, casts: {}".format(self.name, self.school, self.level, self.cast_limit, self.effect, self.__casts)
+
+    def cast(self):
+        if self.__casts > 0:
+            # This means we CAN cast it
+            self.__casts -= 1
+            print("Casting '{}'...".format(self.name))
+            print(self.effect)
+            print("You can cast '{}' {} more time(s) today.".format(self.name, self.__casts))
+        else:
+            # We can't
+            print("Can't cast '{}' any more today.".format(self.name))
+
+
+    def recharge(self):
+        self.__casts = self.cast_limit
+
+
+    def get_casts(self):
+        """
+        Getter Function: Returns self.__casts
+        Read Access Only
+        """
+        return self.__casts
+
+    def set_casts(self, casts):
+        """
+        :param casts: Number of casts
+        Setter Function: Sets self.__casts to param:casts
+        Write Access Only
+        """
+        self.__casts = casts
