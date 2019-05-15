d1e9d4ce4ec1a1ef91b44075cc2e0529265edd50
Max Fisher <machfour+git@protonmail.com>
2019-05-15 16:21:33 +1000

    add iterative fibonacci w15a

--- week11/W15A/fibonacci.py
+++ week11/W15A/fibonacci.py
@@ -13,4 +13,30 @@ def fibonacci(n):
         # F(n) = F(n-1) + F(n-2)
         return fibonacci(n-1) + fibonacci(n-2)
 
+def fibonacci_iterative(n):
+    if n < 0:
+        raise ValueError("n must be >= 0")
+    # base cases
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # else
+    # i.e. F(n-2) and F(n-1)
+    two_steps_back = 0
+    one_step_back = 1
+
+    # we just need to repeat the Fibonacci step the
+    # correct number of times
+    i = n - 1 # n = 2 -> i starts at 1
+    while i > 0: # n = 2 -> loop iterates once
+        # fibonacci step: F(n) = F(n-1) + F(n-2)
+        next_number = one_step_back + two_steps_back
+        # update the previous steps
+        two_steps_back = one_step_back
+        one_step_back = next_number
+        i -= 1
+    return next_number
+
+
 
