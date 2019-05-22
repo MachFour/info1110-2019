10b51d03fb2783ddfabbe272dff84b9def54c524
bitnahian <nhas9102@uni.sydney.edu.au>
2019-05-22 14:47:12 +1000

    Adding last numpy files

--- week12/W13B/transpose.py
+++ week12/W13B/transpose.py
@@ -15,18 +15,37 @@ The transpose of the above matrix `A` is:
 2 4 6
 """
 
+image = [
+            [
+                [1,2], # R
+                [3,4]
+            ],
+
+            [
+                [0,255], # G
+                [0,5]
+            ],
+
+            [
+                [3,2], # B 
+                [199,1]
+            ]
+
+        ]
+
+image_numpy = np.array(image, dtype=np.float64)
 
 #for i in range(len(matrix[0])):
 #    for j in range(len(matrix)):
 #        print(matrix[j][i], end=' ')
 #    print()
 
-matrix_n = np.array(matrix, dtype=np.int32)
+#matrix_n = np.array(matrix, dtype=np.int32)
 
-print(matrix_n)
+#print(matrix_n)
 
-print(matrix_n.T)
+#print(matrix_n.T)
 
-a = np.matmul(matrix_n, matrix_n.T)
+#a = np.matmul(matrix_n, matrix_n.T)
 
-print(matrix_n * matrix_n.T)
+#print(matrix_n * matrix_n.T)
